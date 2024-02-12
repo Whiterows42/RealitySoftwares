bbe39d8e6572282a5f26925f48bb8eea5f758aad
Ajit Mote <123333490+Whiterows42@users.noreply.github.com>
2024-02-12 06:16:38 +0530

    bradcrump updated

--- src/Componant/Breadcrumb/Breadcrumb.jsx
+++ src/Componant/Breadcrumb/Breadcrumb.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 import "./Breadcrumb.css";
-import { Link,NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 export default function Breadcrumb({ bg, title }) {
   return (
-    <div className="vh-100 vw-100 ">
+    <div className="w-100 position-relative  v-100 ">
       <div className="image">
         <img
           src={bg}
           alt=""
-          className="d-block vw-100 vh-100 overflow-hidden  object-fit-cover"
+          className="d-block w-100  img-fluid  object-fit-contain "
         />
       </div>
       <div className="bradum d-flex justify-content-center align-items-center text-white ">
         <div className="row ">
-          <div className="col-md-12">
+          <div className="col-md-12 d-flex flex-column ">
             <h1 className="text-center ">{title}</h1>
             <ul className="breadcrumb">
               <li>
-                <Link className="text-white " to={"/"}>
+                <Link
+                  className="text-white  text-decoration-none fw-bold "
+                  to={"/"}
+                >
                   <i className="fa fa-home"></i> Home
                 </Link>
               </li>
-              <li className="active">{title}</li>
+              <li className="active fw-bold text-decoration-none d-flex align-items-center text-white-50   ">
+                {title}
+              </li>
             </ul>
           </div>
         </div>
